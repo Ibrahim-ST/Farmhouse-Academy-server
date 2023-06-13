d59cae69e247f39fe3f2e1918a57b137cfd3cc2e
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-06-14 01:00:36 +0600

    integrated jwt and jwtverify route

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+
 require("dotenv").config();
 
 
 //middleware 
 app.use(cors())
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
@@ -33,11 +52,19 @@ async function run() {
     const classesCollection = client.db("farmhouseDb").collection("classes");
     const cartCollection = client.db("farmhouseDb").collection("carts");
 
+    //jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+
     // users related apis
-   /*  app.get('/users',   async (req, res) => {
+    app.get('/users',   async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    }); */
+    });
 
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -51,6 +78,19 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     app.get('/classes', async (req, res) => {
       const result = await classesCollection.find().toArray();
